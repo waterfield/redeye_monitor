4d460bf44be7ef079fa9af827c5bc2686c232761
Nathan Matthews <lowentropy@gmail.com>
2012-03-09 11:43:42 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedeyeMonitor::Application.config.secret_token = '0b8686b9e4ae8b9e3ba02b2ddc0f495ece6e6f2b158c25046bb8d44a633c534e2b04e2207c8cbf7f90226f885cd0bf3f1af1746ebb2c30849984377bfdba15cc'
